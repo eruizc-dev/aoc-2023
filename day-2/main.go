a9f7d6e100de93e85816b2e3ba7e6e5ab078fb06
Emiliano Ruiz Carletti <contact@eruizc.dev>
2023-12-02 02:55:58 -0300

    Another star

--- day-2/main.go
+++ day-2/main.go
@@ -24,12 +24,15 @@ func main() {
     document := read("./input.txt")
     games := parse(document)
     ids := 0
+    ids_2 := 0
     for _, game := range games {
         if game.Bag['r'] <= 12 && game.Bag['g'] <= 13 && game.Bag['b'] <= 14 {
             ids += game.Id
         }
+        ids_2 += game.Bag['r'] * game.Bag['g'] * game.Bag['b']
     }
     fmt.Printf("1. Result: %d\n", ids)
+    fmt.Printf("2. Result: %d\n", ids_2)
 }
 
 func read(filename string) []string {
@@ -65,6 +68,31 @@ func parse(s []string) []Game {
                 var color rune
                 fmt.Sscanf(set, "%d %c", &count, &color)
                 if games[i].Bag[color] < count {
+                    games[i].Bag[color] = count
+                }
+            }
+        }
+    }
+    return games
+}
+
+func parse_2(s []string) []Game {
+    games := make([]Game, len(s))
+    for i, line := range s {
+        if len(line) == 0 {
+            break
+        }
+
+        var id int
+        fmt.Sscanf(line, "Game %d:", &id)
+        bag := strings.Split(line, ":")[1]
+        games[i] = NewGame(id)
+        for _, sets := range strings.Split(bag, ";") {
+            for _, set := range strings.Split(sets, ",") {
+                var count int
+                var color rune
+                fmt.Sscanf(set, "%d %c", &count, &color)
+                if games[i].Bag[color] > count {
                     games[i].Bag[color] = count
                 }
             }
